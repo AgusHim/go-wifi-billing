12fb8644f5aa17470437c5b01f1bc85e72a4a1d3
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-11-02 14:34:47 +0700

    feat: add cors

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/Agushim/go_wifi_billing/services"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -71,6 +72,12 @@ func main() {
 
 	// Setup Fiber
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+    AllowOrigins: "*", // allow all origins, or specify like "http://localhost:3000"
+    AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+    AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
+
 
 	// Register routes for all controllers
 	routes.Setup(app, coverageCtrl, userCtrl, packageCtrl, odcCtrl, odpCtrl, customerCtrl, subscriptionCtrl, billCtrl)
